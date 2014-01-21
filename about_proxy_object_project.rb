ce34654e3e607be58c917a47cfffd6274dc68e66
Michael Young <michaeljy@gmail.com>
2014-01-20 16:35:13 -0800

    more done

--- about_proxy_object_project.rb
+++ about_proxy_object_project.rb
@@ -16,9 +16,26 @@ class Proxy
   def initialize(target_object)
     @object = target_object
     # ADD MORE CODE HERE
+    @messages = []
   end
 
   # WRITE CODE HERE
+   def method_missing(method_name, *args, &block)
+        @messages << method_name
+        @object.send method_name, *args, &block
+   end
+
+   def messages
+        @messages
+   end
+
+   def called? method_name
+        @messages.include? method_name
+   end
+
+   def number_of_times_called method_name
+        @messages.select { |m| m == method_name }.size
+   end
 end
 
 # The proxy object should pass the following Koan:
